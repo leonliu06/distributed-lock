5897ac9c8343e4488fda49466bb1050a8b2d9fb5
leonliu06 <leonliu06@gmail.com>
2018-02-02 14:06:58 +0800

    unit test redislock

--- src/test/java/net/mrliuli/lock/RedisLockTest.java
+++ src/test/java/net/mrliuli/lock/RedisLockTest.java
@@ -2,11 +2,17 @@ package net.mrliuli.lock;
 
 import net.mrliuli.BaseTest;
 import net.mrliuli.config.LockConfig;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by li.liu on 2018/1/29.
@@ -18,23 +24,52 @@ public class RedisLockTest extends BaseTest{
 
     @Test
     @Transactional
-    @Rollback(false)
-    public void testLock() throws Exception {
+    @Rollback
+    public void testLock() {
 
         final RedisLock redisLock = new RedisLock(redisTemplate, new LockConfig(0, 2000));
 
-        for(int i = 0; i < 10; i++){
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        final int COUNT = 50;
+        final CountDownLatch currentThreadLatch = new CountDownLatch(COUNT);
+
+        final List lockList = new ArrayList();
+
+        for(int i = 0; i < COUNT; i++){
             new Thread(new Runnable() {
                 @Override
                 public void run() {
+                    try{
+                        countDownLatch.await();
+                    }catch (InterruptedException e){
+                        System.out.println(e);
+                    }
                     boolean locked = redisLock.lock("testKey");
-                    System.out.println(Thread.currentThread().getName() + (locked ? "获得了锁" : "没有获得锁"));
-                    //redisLock.unlock("testKey");
+                    System.out.println(Thread.currentThread().getName() + "\t" + (locked ? "得到锁" : "未得到锁"));
+
+                    if(locked){ lockList.add(1); }
+
+                    currentThreadLatch.countDown();
                 }
             }).start();
         }
 
-        Thread.sleep(10000);
+        System.out.println("线程准备完毕");
+        countDownLatch.countDown();
+
+        try {
+            currentThreadLatch.await();
+        }catch (InterruptedException e){
+            System.out.println(e);
+        }
+
+        System.out.println("=================================end");
+
+        System.out.println("释放锁");
+        redisLock.unlock("testKey");
+
+        Assert.assertTrue(lockList.size() <= 1);
 
     }
 
